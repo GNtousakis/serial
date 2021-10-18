a685af44ba142b3ed3e1cc3c5f71d9546f84b9f2
GNtousakis <greco1996@gmail.com>
2021-10-18 15:27:16 -0400

    Update

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+var debug = process['env']['DEBUG'];
+
 module.exports = { 
   dec: (str) => {
     let obj;
     obj = eval('('+ str + ')');
+    if (debug) {
+      console.log(check);
+    }
     return obj;
   }
 }
